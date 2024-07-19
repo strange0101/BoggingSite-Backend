6e1a56ff7032f462d3939ef8d72e5116da4cad8f
Aryan Sahu <121814870+strange0101@users.noreply.github.com>
2024-07-19 11:26:19 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://prismatic-cassata-0713ec.netlify.app/'
+    'https://prismatic-cassata-0713ec.netlify.app'
 
 ];
 
